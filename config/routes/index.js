c2750a8d3425b41eb275b44849019fa186410c58
Fannny Arif Nasrudin <109141738+fanchann@users.noreply.github.com>
2022-09-28 15:26:44 +0000

    Update index.js

--- config/routes/index.js
+++ config/routes/index.js
@@ -5,8 +5,6 @@ const translateMethod = require('./modules/modules')
 /* GET home page. */
 router.get('/api/', translateMethod.index)
 router.get('/api/translate/to/:to/:text', translateMethod.autoDetectTranslate)
-// router.get('/api/docs', translateMethod.documentation)
 router.get('/api/translate/from/:from/to/:to/:text', translateMethod.selectLangTranslate)
 router.all('*', translateMethod.nothing)
-
 module.exports = router;
